2970141839034cc0b2a3fc50379405eafea750a5
micson-us <micson@deloitte.com>
2021-06-18 13:45:10 -0400

    removed port 80 from description

--- aws_xia_sg.tf
+++ aws_xia_sg.tf
@@ -1,7 +1,7 @@
 # security group to allow inbound connection on port 8000 from VPC cidr. 443&80 from anywhere
 resource "aws_security_group" "ecs_xia_sg" {
   name        = "ecs_xia_sg"
-  description = "Allow TLS inbound traffic on ports 8000,8010,8020,80,443"
+  description = "Allow TLS inbound traffic on ports 8000,8010,8020,443"
   vpc_id      = aws_vpc.openlxp_vpc.id
 
   ingress {
